1df08613345ef53ca7c9796b8ef1a2300d2f686f
Divay Prakash <divayprakash0@gmail.com>
2020-03-31 12:58:01 +0530

    Set page title to day name

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ let k3 = [20, 22, 23, 28, 29, 30],
     k10 = [10, 12, 13, 14, 15, 16, 17, 18, 19, 31],
     k13 = [11];
 let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-let days = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
+let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 let firstDay = new Date(currentYear, currentMonth, 1).getDay();
 let firstDayWeekday = firstDay == 0 ? 7 : firstDay;
 let monthLength = 32 - new Date(currentYear, currentMonth, 32).getDate();
 let html = '<h2 class="year">' + currentYear + '</h2>';
 html += '<h2 class="month">' + months[currentMonth] + '</h2>';
 html += '<table><thead><tr>';
-for (let i = 0; i <= 6; i++) html += '<td>' + days[i] + '</td>';
+for (let i = 0; i <= 6; i++) html += '<td>' + days[i].substring(0, 1) + '</td>';
 html += '</tr></thead>';
 let day = 1;
 html += '<tbody><tr>';
@@ -48,6 +48,7 @@ for (let i = 0; i < 7; i++) {                                           // weeks
 }
 html += '</tbody></table>';
 document.getElementById('calendar').innerHTML = html;
+document.title = days[(today.getDay() + 1) % 7];
 window.addEventListener('load', (event) => {
     document.body.classList.add('ready');
 });
